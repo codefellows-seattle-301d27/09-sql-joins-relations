723db7cdda107d5e0774f5da8bf9c3088ea5ed63
DocJek <bassroscoe@gmail.com>
2017-07-21 11:11:14 -0700

    Added query to update article record on server.js line 94

--- starter-code/server.js
+++ starter-code/server.js
@@ -77,20 +77,24 @@ app.post('/articles', function(request, response) {
 });
 
 app.put('/articles/:id', function(request, response) {
-  // TODO: Write a SQL query to update an author record. Remember that our articles now have
+  // DONE TODO: Write a SQL query to update an author record. Remember that our articles now have
   // an author_id property, so we can reference it from the request.body.
-  // TODO: Add the required values from the request as data for the SQL query to interpolate
+  // DONE TODO: Add the required values from the request as data for the SQL query to interpolate
   client.query(
-    ``,
-    []
+    `INSERT INTO articles (author_id, title, category, "publishedOn", body) VALUES ($1, $2, $3, $4, $5);`,
+    [
+      request.body.author_id
+    ]
   )
   .then(function() {
     // TODO: Write a SQL query to update an article record. Keep in mind that article records
     // now have an author_id, in addition to title, category, publishedOn, and body.
     // TODO: Add the required values from the request as data for the SQL query to interpolate
     client.query(
-      ``,
-      []
+      `UPDATE articles SET author_id = $1, title = $2, category = $3, "publishedOn" = $4, body = $5;`,
+      [
+        
+      ]
     )
   })
   .then(function() {
