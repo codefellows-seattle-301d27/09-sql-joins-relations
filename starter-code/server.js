3a05add0893a2c7de406098f1f3cb73ab13dddd9
dmuldrew <dmuldrew@gmail.com>
2017-07-20 18:06:20 -0700

    added queries to retrieve all article data and insert new author

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = '';// TODO: Don't forget to set your own conString
+const conString = 'postgres://localhost:5432';// TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
@@ -24,7 +24,9 @@ app.get('/new', function(request, response) {
 app.get('/articles', function(request, response) {
   // REVIEW: This query will join the data together from our tables and send it back to the client.
   // TODO: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
-  client.query(``)
+  client.query(`SELECT * FROM articles
+                INNER JOIN authors ON
+                articles.author_id = authors.author_id`)
   .then(function(result) {
     response.send(result.rows);
   })
@@ -34,9 +36,13 @@ app.get('/articles', function(request, response) {
 });
 
 app.post('/articles', function(request, response) {
+  // TODO: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
   client.query(
-    '', // TODO: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
-    [], // TODO: Add the author and "authorUrl" as data for the SQL query
+    `INSERT IN author (author, authorUrl), VALUES ($1, $2) ON CONFLICT DO NOTHING;`,
+    [
+      req.body.author_id,
+      req.body.authorUrl
+    ], // TODO: Add the author and "authorUrl" as data for the SQL query
     function(err) {
       if (err) console.error(err)
       queryTwo() // This is our second query, to be executed when this first query is complete.
