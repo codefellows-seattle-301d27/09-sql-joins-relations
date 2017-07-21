034f1c8a635986db9aa57eef265737fa85403131
J Cheyo Jimenez <cheyo@masters3d.com>
2017-07-20 17:30:14 -0700

    added code to insert

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const conString = 'postgres://localhost:5432/kilovolt';// TODO: Don't forget to set your own conString. IT took 4 minutes
+const conString = 'postgres://localhost:5432/kilovolt';// DONE: Don't forget to set your own conString. IT took 4 minutes
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
@@ -23,7 +23,7 @@ app.get('/new', function(request, response) {
 
 app.get('/articles', function(request, response) {
   // REVIEW: This query will join the data together from our tables and send it back to the client.
-  // TODO: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
+  // DONE: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
   // Estimated 15 minutes
   // It took: 10 mins
   client.query(`select * from articles left join authors on authors.author_id=articles.author_id;`)
@@ -37,8 +37,13 @@ app.get('/articles', function(request, response) {
 
 app.post('/articles', function(request, response) {
   client.query(
-    '', // TODO: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
-    [], // TODO: Add the author and "authorUrl" as data for the SQL query
+    `insert into authors (author, "authorUrl") values ($1, $2) on conflict do nothing;`, // TODO: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
+    // we found the answer here on why it needs quotes:
+    // https://stackoverflow.com/a/563126/3705470
+    // it took 6 minutes 
+    [request.body.author, request.body.authorUrl],
+    // DONE: Add the author and "authorUrl" as data for the SQL query
+    // It took 4 mins
     function(err) {
       if (err) console.error(err)
       queryTwo() // This is our second query, to be executed when this first query is complete.
