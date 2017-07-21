758173b26b230f63fa94344a64a60fbc549ccd14
Sara Jade <wisconsinsara@gmail.com>
2017-07-20 23:30:00 -0700

    Update server.js to query for author_id of new article

--- starter-code/server.js
+++ starter-code/server.js
@@ -53,11 +53,15 @@ app.post('/articles', function(request, response) {
 
   function queryTwo() {
     client.query(
-      ``, // TODO: Write a SQL query to retrieve the author_id from the authors table for the new article
-      [], // TODO: Add the author name as data for the SQL query
+      `SELECT author_id FROM authors WHERE author = '${request.body.author}' AND "authorUrl" = '${request.body.authorUrl}';`,
+       // DONE: Write a SQL query to retrieve the author_id from the authors table for the new article
+       // est: 10min act: 40min
+       // DONE: Add the author name as data for the SQL query
+       // est: 1min act: 1min
       function(err, result) {
         if (err) console.error(err)
-        queryThree(result.rows[0].author_id) // This is our third query, to be executed when the second is complete. We are also passing the author_id into our third query
+        queryThree(result.rows[0].author_id) // This is our third query, to be executed when the second is complete.
+        // We are also passing the author_id into our third query
       }
     )
   }
